949e58d490f1821917de7c20201779c2d60dd950
JacU1 <jacunio122@gmail.com>
2024-02-22 20:32:16 +0100

    Geolocation api + new services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-weather-app';
+
+  constructor() {
+    this.askForLocation();
+  }
+
+  askForLocation() {
+    if (navigator.geolocation) {
+      console.log(navigator.geolocation);
+      navigator.geolocation.getCurrentPosition(this.showPosition, this.showError);
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
+
+  showPosition(res: any): void {
+    console.log(res);
+  }
+
+  showError(error: any) {
+    console.log(error);
+    switch(error.code) {
+      case error.PERMISSION_DENIED:
+        alert("User denied the request for Geolocation.");
+        break;
+      case error.POSITION_UNAVAILABLE:
+        alert("Location information is unavailable.");
+        break;
+      case error.TIMEOUT:
+        alert("The request to get user location timed out.");
+        break;
+      case error.UNKNOWN_ERROR:
+        alert("An unknown error occurred.");
+        break;
+    }
+  }
 }
