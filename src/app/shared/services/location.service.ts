a50e55b4dfb7133f1476be771aab62461ceb79ff
JacU1 <jacunio122@gmail.com>
2024-03-03 22:18:13 +0100

    Relocate location to service

--- src/app/shared/services/location.service.ts
+++ src/app/shared/services/location.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class LocationService {
 
   constructor() { }
+
+  getCurrentLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.showPosition, this.showError);
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
+
+  showPosition(res: GeolocationPosition): GeolocationCoordinates {
+    console.log(res);
+    return res.coords;
+  }
+
+  showError(error: any) {
+    switch(error.code) {
+      case error.PERMISSION_DENIED:
+        alert("User denied the request for Geolocation.");
+        break;
+      case error.POSITION_UNAVAILABLE:
+        alert("Location information is unavailable.");
+        break;
+      case error.TIMEOUT:
+        alert("The request to get user location timed out.");
+        break;
+      case error.UNKNOWN_ERROR:
+        alert("An unknown error occurred.");
+        break;
+    }
+  }
 }
