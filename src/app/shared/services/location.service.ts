125c23d2ef8c3e3b2aa8a84dcac329680a02880a
JacU1 <jacunio122@gmail.com>
2024-03-07 22:24:38 +0100

    Fix subject in service

--- src/app/shared/services/location.service.ts
+++ src/app/shared/services/location.service.ts
@@ -1,36 +1,41 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class LocationService {
-
-  constructor() { }
+  public currentPosition$: Subject<GeolocationPosition> = new Subject<GeolocationPosition>;
 
   getCurrentLocation() {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(this.showPosition, this.showError);
+      navigator.geolocation.getCurrentPosition(
+        this.showPosition,
+        this.showError
+      );
     } else {
-      alert("Geolocation is not supported by this browser.");
+      alert('Geolocation is not supported by this browser.');
     }
   }
 
-  showPosition(res: GeolocationPosition): GeolocationCoordinates {
+  showPosition(res: GeolocationPosition): GeolocationPosition {
     console.log(res);
-    return res.coords;
+    console.log(this.currentPosition$);
+    this.currentPosition$.next(res);
+    return res;
   }
 
   showError(error: any) {
-    switch(error.code) {
+    switch (error.code) {
       case error.PERMISSION_DENIED:
-        alert("User denied the request for Geolocation.");
+        alert('User denied the request for Geolocation.');
         break;
       case error.POSITION_UNAVAILABLE:
-        alert("Location information is unavailable.");
+        alert('Location information is unavailable.');
         break;
       case error.TIMEOUT:
-        alert("The request to get user location timed out.");
+        alert('The request to get user location timed out.');
         break;
       case error.UNKNOWN_ERROR:
-        alert("An unknown error occurred.");
+        alert('An unknown error occurred.');
         break;
     }
   }
