08fb5a6d6a8efc546f34fca6c0fcf1c93f0a1cca
JacU1 <jacunio122@gmail.com>
2024-07-04 22:29:18 +0200

    Add lang

--- src/app/views/main-view/smart-components/today-weather/today-weather.component.ts
+++ src/app/views/main-view/smart-components/today-weather/today-weather.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { WeatherTypesEnum } from 'src/app/shared/models/weather';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @Component({
   selector: 'app-today-weather',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './today-weather.component.html',
   styleUrl: './today-weather.component.scss'
 })
