ebc87749d40df4d1fc9500dac2b8c2fa905357eb
Jacek <jacunio122@gmail.com>
2024-07-24 21:41:36 +0200

    Font changes + some other idk

--- src/app/views/main-view/dumb-components/today-weather/today-weather.component.ts
+++ src/app/views/main-view/dumb-components/today-weather/today-weather.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { WeatherTypesEnum } from 'src/app/shared/models/weather';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { DayBoxComponent } from "../day-box/day-box.component";
 
 @Component({
   selector: 'app-today-weather',
   standalone: true,
-  imports: [SharedModule],
+  imports: [SharedModule, DayBoxComponent],
   templateUrl: './today-weather.component.html',
   styleUrl: './today-weather.component.scss'
 })
